4b1c77d6407667326419e66877d0449fa00edf57
davidcubero19 <davidcubero79@gmail.com>
2025-05-08 10:49:56 -0600

    Creacion de pagina SignUp

---
+++ cypress/e2e/pages/login/signUp/signup.methods.js
@@ -0,0 +1,22 @@
+import { SignUpElements } from "./signup.element";
+
+export class SignUpMethods{
+    static insertUsername(username){
+        SignUpElements.textboxes.username.invoke("val", username)
+    }
+
+    static insertPassword(password){
+        SignUpElements.textboxes.password.invoke("val", password)
+    }
+
+    static clickOnSignupButton(){
+        SignUpElements.buttons.signup.click()
+    }
+
+    static signup(username, password){
+        this.insertUsername(username);
+        this.insertPassword(password);
+        this.clickOnSignupButton();
+    }
+
+}

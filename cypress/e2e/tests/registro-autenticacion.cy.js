876716c5539dc9c2bfdc4ddeb106c516870ffcc0
davidcubero19 <davidcubero79@gmail.com>
2025-05-13 08:29:11 -0600

    Crear usuario aleatorio

--- cypress/e2e/tests/registro-autenticacion.cy.js
+++ cypress/e2e/tests/registro-autenticacion.cy.js
@@ -3,6 +3,9 @@ import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { SignUpMethods } from "../pages/signUp/signup.methods";
 import { Logger } from "../util/logger";
 
+const user = CommonPageMethods.generateRandomString(10);
+const contra = CommonPageMethods.generateRandomString(7)
+
 describe (CommonPageData.testSuites.registroYAutenticacion, () => {
     it ("Registro de usuario válido", () => {
         Logger.stepNumber(1)
@@ -15,8 +18,8 @@ describe (CommonPageData.testSuites.registroYAutenticacion, () => {
 
         Logger.stepNumber(3)
         Logger.step('Completar los campos obligatorios con información válida')
-        SignUpMethods.insertUsername("jsklhfdshsd");
-        SignUpMethods.insertPassword("jsklhfdshsd");
+        SignUpMethods.insertUsername(user);
+        SignUpMethods.insertPassword(contra);
 
         Logger.stepNumber(4)
         Logger.step('Hacer click en sign up para registrar usuario')
